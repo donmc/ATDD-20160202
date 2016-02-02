1644f90cf6746d701a1fb0800a255c1eaf1c8d69
it94879(phillip) <it94879@L6042860.eagle.usaa.com>
2016-02-02 14:16:18 -0600

    Added tests for Flight class

---
+++ TddAir/test/com/tddair/FlightTest.java
@@ -0,0 +1,53 @@
+package com.tddair;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlightTest {
+
+	@Test
+	public void testGetFullFlightNumber() {
+		// setup
+		Flight flight = new Flight("SAT", "DFW", 300, "SW", 32);
+		// exercise
+		String fullFlightNumber = flight.getFullFlightNumber();
+		// verify
+		assertEquals("SW32", fullFlightNumber);
+	}
+
+	@Test
+	public void testGetFullFlightNumber_Unknown() {
+		// setup
+		Flight flight = new Flight("SAT", "DFW", 300, null, 0);
+		// exercise
+		String fullFlightNumber = flight.getFullFlightNumber();
+		// verify
+		assertEquals("UNKNOWN", fullFlightNumber);
+	}
+	
+	@Test
+	public void testGetFullFlightNumber_UnknownNumber() {
+		// setup
+		Flight flight = new Flight("SAT", "DFW", 300, "SW", 0);
+		// exercise
+		String fullFlightNumber = flight.getFullFlightNumber();
+		// verify
+		assertEquals("UNKNOWN", fullFlightNumber);
+	}
+	
+	@Test
+	public void testGetFlightInformation(){
+		// setup
+		String airline = "SW";
+		int number = 32;
+		Flight flight = new Flight("SAT", "DFW", 300, airline, number);
+		// exercise
+		String flightInfo = flight.getInformation();
+		// verify
+		assertEquals("SW32 | SAT -> DFW | 300 miles", flightInfo);
+		
+	}
+}
+
+
