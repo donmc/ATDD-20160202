6aaa73c45af95e19d405052fa1011477b2b542f3
Ankur@USAA <no-reply@usaa.com>
2016-02-03 14:44:27 -0600

    added serial version id

--- TddAir/src/com/tddair/DuplicateUsernameException.java
+++ TddAir/src/com/tddair/DuplicateUsernameException.java
@@ -2,6 +2,11 @@ package com.tddair;
 
 public class DuplicateUsernameException extends Exception {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7782164149118059932L;
+
 	public DuplicateUsernameException()
 	{
 		super("Username already exists");
--- TddAir/src/com/tddair/InvalidEmailException.java
+++ TddAir/src/com/tddair/InvalidEmailException.java
@@ -2,6 +2,11 @@ package com.tddair;
 
 public class InvalidEmailException extends Exception {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6206979301561203548L;
+
 	public InvalidEmailException()
 	{
 		super("Email is Invalid!");
